402be8b3c3a71126c86c1fd3e9333c3ce2da3923
SamEsteb <samuelcoj123.a@gmail.com>
2025-04-06 16:29:29 -0400

    Test3: Código mínimo para que pase - SUCCESS

--- src/manhattan_distance.py
+++ src/manhattan_distance.py
@@ -4,4 +4,7 @@ class Point:
         self.y = y
 
 def manhattan_distance(point1, point2):
-    return point1.x -point2.x
+    diff_x = point1.x - point2.x
+    if diff_x < 0:
+        diff_x = -diff_x
+    return diff_x
