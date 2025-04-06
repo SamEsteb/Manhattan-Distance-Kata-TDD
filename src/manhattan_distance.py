89edfec8d8b5b2f9bb32ba6b804ce72757b28d3c
SamEsteb <samuelcoj123.a@gmail.com>
2025-04-06 16:35:36 -0400

    Test5: Refactorizar usando Valor absoluto en vez de If's

--- src/manhattan_distance.py
+++ src/manhattan_distance.py
@@ -5,7 +5,5 @@ class Point:
 
 def manhattan_distance(point1, point2):
     diff_x = abs(point1.x - point2.x)
-    diff_y = point1.y - point2.y
-    if diff_y < 0:
-        diff_y = -diff_y
+    diff_y = abs(point1.y - point2.y)
     return diff_x + diff_y
