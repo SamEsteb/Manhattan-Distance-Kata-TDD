3b168bdb26542707ef20ec3eab26ad70e3f1d800
SamEsteb <samuelcoj123.a@gmail.com>
2025-04-06 16:25:17 -0400

    Test2: 2 puntos distintos positivos en Eje X, punto1.x>punto2.x - FAIL

--- src/tests/test_manhattan_distance.py
+++ src/tests/test_manhattan_distance.py
@@ -5,3 +5,8 @@ def test_manhattan_distance_2_puntos_iguales():
     point1 = Point(1, 2)
     point2 = Point(1, 2)
     assert manhattan_distance(point1, point2) == 0
+
+def test_manhattan_distance_2_puntos_distintos_eje_X_punto1x_mayor_punto2x():
+    point1 = Point(9, 0)
+    point2 = Point(5, 0)
+    assert manhattan_distance(point1, point2) == 4
